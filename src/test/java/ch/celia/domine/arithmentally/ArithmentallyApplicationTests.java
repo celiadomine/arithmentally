d015966602cb16e4ebe9714fe9acbc7866a6d187
celia.domine <celia.domine@outlook.de>
2025-04-04 14:28:23 +0200

    initial commit

---
+++ src/test/java/ch/celia/domine/arithmentally/ArithmentallyApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.celia.domine.arithmentally;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArithmentallyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
